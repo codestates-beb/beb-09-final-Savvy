f1cca88a2b32926aa1c29461c9f0ecd6f7beae27
canu <cksdncjf@naver.com>
2023-07-24 01:36:18 +0900

    Add: detect metamask

---
+++ client/src/api/post-createTba.js
@@ -0,0 +1,3 @@
+import axios from "axios";
+
+export const createTba = async (address, nftId) => {};
--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const routes = require('./routes');
-const cookieParser = require('cookie-parser');
-const { swaggerUi, specs } = require('./swagger/swagger');
-const cors = require('cors');
+const routes = require("./routes");
+const cookieParser = require("cookie-parser");
+const { swaggerUi, specs } = require("./swagger/swagger");
+const cors = require("cors");
 
-require('dotenv').config();
+require("dotenv").config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,17 +13,17 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ["http://localhost:3000"],
     credentials: true,
   })
 );
 
-app.use('/', routes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/", routes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 const PORT = process.env.PORT || 8080;
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // const userInfo = require('./config/userinfo.json');
 
 mongoose
@@ -33,8 +33,8 @@ mongoose
     user: process.env.MONGODB_USERNAME,
     pass: process.env.MONGODB_PASSWORD,
   })
-  .then(() => console.log('MongoDB에 연결되었습니다.'))
-  .catch((err) => console.error('MongoDB에 연결할 수 없습니다.', err));
+  .then(() => console.log("MongoDB에 연결되었습니다."))
+  .catch((err) => console.error("MongoDB에 연결할 수 없습니다.", err));
 
 module.exports = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
