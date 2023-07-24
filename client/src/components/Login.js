6791bf9031bd74f9637c7ca90bde94eae134fe1d
23123121312312312323 <hot00has@naver.com>
2023-07-24 17:57:53 +0900

    Add: loginpage

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom"; 
 
 export default function Login() {
-  return <h1>Hello from Login Page</h1>;
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);  
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+      setWindowHeight(window.innerHeight);  
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const containerStyle = {
+    display: "flex",
+    justifyContent: "flex-end",  
+    alignItems: "center",
+    height: "100%", 
+    position: "relative", 
+  };
+
+  const videoStyle = {
+    width: "550px",  
+    height: windowHeight > 850 ? `${windowHeight}px` : "100%", 
+    objectFit: "cover",
+    objectPosition: windowWidth > windowHeight ? "center" : "top",  
+    display: windowWidth < 1175 ? "none" : "block",
+    boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.7)",
+    borderTopLeftRadius: "50px",    
+    borderBottomLeftRadius: "50px",  
+  };
+ 
+  const logoStyle = {
+    position: "absolute", 
+    top: "0",          
+    left: "0",          
+    width: "100px",
+    height: "auto",
+    margin: "1rem",  
+  };
+
+  return (
+    <div style={containerStyle}>
+      <Link to="/"> 
+        <img src="/logoblack.png" alt="Logoblack" style={logoStyle} />
+      </Link>
+      <video src="/login.mp4" autoPlay loop muted style={videoStyle} />
+    </div>
+  );
 }
--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Web3Auth } from '@web3auth/modal';
 import Login from '../components/Login.js';
-import Signup from '../components/Signup.js';
 import ethersRPC from '../ethersRPC.js';
 import { getPublicCompressed } from '@toruslabs/eccrypto';
 
@@ -11,22 +10,24 @@ import { postLogin } from '../api/post-login.js';
 export default function AuthPage() {
   const [isLogin, setIsLogin] = useState(false);
   const [provider, setProvider] = useState(null);
+  const [screenSize, setScreenSize] = useState('');
 
   const web3Auth = new Web3Auth({
     clientId: `${process.env.REACT_APP_WEB3_CLIENT_ID}`,
     chainConfig: {
       chainNamespace: 'eip155',
-      chainId: '0xAA36A7', // 0x1(mainet)
-      rpcTarget: `https://rpc.ankr.com/eth_sepolia`, // https://rpc.ankr.com/eth(mainet)
+      chainId: '0xAA36A7',
+      rpcTarget: `https://rpc.ankr.com/eth_sepolia`,
     },
   });
 
   useEffect(() => {
     const init = async () => {
       await web3Auth.initModal();
+      document.body.classList.add('white-bg-page');
     };
     init();
-  });
+  }, []);
 
   useEffect(() => {
     const login = async () => {
@@ -39,7 +40,7 @@ export default function AuthPage() {
         const { email, name, profileImage, idToken } = userInfo;
         localStorage.setItem('token', idToken);
         const app_scoped_privkey = await web3Auth.provider?.request({
-          method: 'eth_private_key', // use "private_key" for other non-evm chains
+          method: 'eth_private_key',
         });
         const app_pub_key = getPublicCompressed(
           Buffer.from(app_scoped_privkey.padStart(64, '0'), 'hex')
@@ -58,12 +59,17 @@ export default function AuthPage() {
       }
     };
     login();
-  }, [isLogin]);
+  }, [isLogin, provider]);
 
   const handleLogin = async () => {
-    const web3AuthProvider = await web3Auth.connect();
-    setProvider(web3AuthProvider);
-    setIsLogin(true);
+    try {
+      const web3AuthProvider = await web3Auth.connect();
+      console.log('web3AuthProvider:', web3AuthProvider);
+      setProvider(web3AuthProvider);
+      setIsLogin(true);
+    } catch (error) {
+      console.error('Error during web3Auth.connect():', error);
+    }
   };
 
   const handleLogout = async () => {
@@ -79,14 +85,118 @@ export default function AuthPage() {
     }
   };
 
+  const authPageStyle = {
+    backgroundColor: 'white',
+    position: 'relative',
+  };
+
+  const welcomeTextStyle = {
+    position: 'absolute',
+    top: '440px',
+    left: '260px',
+    transform: 'translate(-50%, -50%)',
+    fontSize: '40px',
+    fontWeight: 'bold',
+    whiteSpace: 'nowrap',
+  };
+
+  const descriptionTextStyle = {
+    position: 'absolute',
+    top: '485px',
+    left: '260px',
+    transform: 'translate(-50%, -50%)',
+    fontSize: '16px',
+    color: '#919091',
+    whiteSpace: 'nowrap',
+  };
+
+  const buttonStyle = {
+    position: 'absolute',
+    top: '560px',
+    width: '300px',
+    height: '50px',
+    background: 'linear-gradient(to right, #576ff6, #a534b7, #d36ae4)',
+    transform: 'translate(-50%, -50%)',
+    padding: '10px 20px',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    color: 'white',
+    borderRadius: '10px',
+    cursor: 'pointer',
+    zIndex: 1,
+    border: 'none',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+  };
+
+  const logoutButtonStyle = {
+    ...buttonStyle,
+    top: '65%',
+    display: 'none',
+    left: '500px',
+  };
+
+  const imageStyle = {
+    position: 'absolute',
+    top: '660px',
+    left: '290px',
+    transform: 'translate(-50%, -50%)',
+    width: '440px',
+    height: 'auto',
+  };
+
+  useEffect(() => {
+    const handleScreenSizeChange = () => {
+      const screenWidth = window.innerWidth;
+      if (screenWidth <= 480) {
+        setScreenSize('extra-small');
+      } else if (screenWidth <= 768) {
+        setScreenSize('small');
+      } else {
+        setScreenSize('');
+      }
+    };
+
+    const smallScreenQuery = window.matchMedia('(max-width: 768px)');
+    const smallerScreenQuery = window.matchMedia('(max-width: 480px)');
+
+    smallScreenQuery.addListener(handleScreenSizeChange);
+    smallerScreenQuery.addListener(handleScreenSizeChange);
+
+    handleScreenSizeChange();
+
+    return () => {
+      smallScreenQuery.removeListener(handleScreenSizeChange);
+      smallerScreenQuery.removeListener(handleScreenSizeChange);
+    };
+  }, []);
+
   return (
-    <div>
-      <h1>Hello from Auth Page</h1>
-      <div>{`${isLogin}`}</div>
-      <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
+    <div style={authPageStyle}>
+      <div style={{...welcomeTextStyle,
+      left: screenSize === 'small' ? '250px' : '460px',
+      }}>Welcome back 👋🏻</div>
+      <div style={{...descriptionTextStyle,
+      left: screenSize === 'small' ? '250px' : '460px',
+      }}>Log in and experience the new service</div>
+      <button
+        onClick={handleLogin}
+        style={{
+          ...buttonStyle,
+          left: screenSize === 'small' ? '250px' : '460px',
+        }}
+      >
+        <span className="login-button">Log in</span>
+      </button>
+      <button
+        onClick={handleLogout}
+        style={{
+          ...logoutButtonStyle,
+        }}
+      >
+        Logout
+      </button>
+      <img src="/loginImage.png" alt="Login Image" style={{...imageStyle, left: screenSize === 'small' ? '278px' : '488px',}} />
       <Login />
-      <Signup />
     </div>
   );
 }
