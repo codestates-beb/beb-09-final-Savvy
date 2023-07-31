0de5de817feb6da835d73372de6638483a3b66eb
canu <cksdncjf@naver.com>
2023-07-31 20:07:48 +0900

    working

--- client/src/api/get-all-tba-group.js
+++ client/src/api/get-all-tba-group.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-export const getAllTbaGroup = async () => {
+export const getAllTbaGroup = async (communityAddress) => {
   try {
     const response = await axios({
       method: "get",
-      url: `http://localhost:8080/tbaAdmin/group/allGroups`,
+      url: `http://localhost:8080/tbaAdmin/group/allGroups/${communityAddress}`,
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
